c388ca6e7394e7b2b931b75d80f54d728f3bb26c
houqiang.xie <1624539850@qq.com>
2022-01-04 23:05:49 +0800

    first commit

---
+++ src/components/nav/NavTitle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-[18px]">我是我大大实打实</div>
+  <div text="[40px] " bg="[#f00]" >dsdsds</div>
+  {{a}}
+</template>
+
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+  const a  = ref<number>(333)
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'virtual:windi.css'
+import 'vue-global-api'
+
+createApp(App).mount('#app')
